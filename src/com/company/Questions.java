c52ee8804b216e58358dd61dbbfd705da9d013eb
Nourhan <nourhanelyamany@gmail.com>
2021-06-06 16:17:15 +0200

    questions class is added

---
+++ src/com/company/Questions.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.*;
+
+public class Questions {
+    List<String> questions = new ArrayList<String>();
+    String answer;
+
+    public void mathQuestions() {
+        questions.add("Solve for q.\n" +
+                "\n" +
+                "–8q = 3 − 11q\n" +
+                "\n" +
+                "q = ");
+        questions.add("Solve for d.\n" +
+                "–\n" +
+                "19d≥20d+3(\n" +
+                "–\n" +
+                "14d+1)\n" +
+                "Write your answer with d first, followed by an inequality symbol.\n" +
+                "\n");
+        questions.add("Solve for h.\n" +
+                "\n" +
+                "–2|h| = –8\n" +
+                "\n" +
+                "\n" +
+                "Write your answers as integers or as proper or improper fractions in simplest form.\n" +
+                "\n" +
+                "h = \n" +
+                " or h = \n ");
+        questions.add("Nolan's retirement party costs\n" +
+                "£\t35\n" +
+                ",plus an additional\n" +
+                "£\t1\n" +
+                "for each guest he invites. What is the maximum number of guests there can be if Nolan can afford to spend a total of\n" +
+                "£\t45\n" +
+                "on his retirement party?");
+        questions.add("–3w + 1 = –4w + 14\n" +
+                "\n" +
+                "w = \n");
+    }
+}
