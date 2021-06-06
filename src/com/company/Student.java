7ef4c5049f25424f327955586dee0d9afbfb15c7
Nourhan <nourhanelyamany@gmail.com>
2021-06-06 18:09:35 +0200

    some changes

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class Student extends Person{
+    int semester;
+    String grade;
+
+    public Student(int id, String username, int semester, String grade) {
+        super(id, username);
+        this.semester = semester;
+        this.grade = grade;
+    }
+
+    public Student(int semester, String grade) {
+        this.semester = semester;
+        this.grade = grade;
+    }
+
+    public Student() {
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", username='" + username + '\'' +
+                ", semester=" + semester +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
 }
