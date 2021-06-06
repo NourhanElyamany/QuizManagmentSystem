32e6e72215f256f73ffe4230a4045f97d441f9ec
Nourhan <nourhanelyamany@gmail.com>
2021-06-06 22:07:28 +0200

    some changes

--- src/com/company/mathCollege.java
+++ src/com/company/mathCollege.java
@@ -4,44 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public abstract class mathCollege implements Questions{
+public class mathCollege extends College{
 
-    List<String> questions = new ArrayList<String>();
+    ArrayList<String> questions = new ArrayList<String>();
 
-    public List<String> mathsQuestions(){
-        questions.add("Solve for q.\n" +
-                "–8q = 3 − 11q\n" + "q = " );
-        questions.add("Solve for j.\n" +
-                "20 + 5j = 2j − 16\n" +
-                "j =");
-        questions.add("Solve for s.\n" +
-                "16 − 19s = –20s\n" +
-                "s =");
-        questions.add("Nolan's retirement party costs £35 ,plus an additional £1 for each guest he invites. What is the maximum number of guests there can be if Nolan can afford to spend a total of £45 on his retirement party?");
-        questions.add("–3w + 1 = –4w + 14\n" + "w = \n");
-        return questions;
-    }
-
-    public mathCollege(List<String> questions) {
+    public mathCollege(String collegeName, int semester, ArrayList<String> questions) {
+        super(collegeName, semester);
         this.questions = questions;
     }
 
     public mathCollege() {
+        this.questions = questions;
     }
 
-    public List<String> getQuestions() {
+    public ArrayList<String> getQuestions() {
         return questions;
     }
 
-    public void setQuestions(List<String> questions) {
+    public void setQuestions(ArrayList<String> questions) {
         this.questions = questions;
+    }
+
+    public ArrayList<String> mathsQuestions(){
+        questions.add("Solve for q.\n –8q = 3 − 11q\n q = ");
+        questions.add("Solve for j.\n 20 + 5j = 2j − 16\n j =");
+        questions.add("Solve for s.\n 16 − 19s = –20s\n s =");
+        questions.add("Nolan's retirement party costs £35 ,plus an additional £1 for each guest he invites. What is the maximum number of guests there can be if Nolan can afford to spend a total of £45 on his retirement party?");
+        questions.add("–3w + 1 = –4w + 14\n w = \n");
+        return questions;
     }
 
     @Override
     public String toString() {
         return "mathCollege{" +
-                "questions=" + mathsQuestions() +
+                "collegeName='" + collegeName + '\'' +
+                ", semester=" + semester +
+                ", questions=" + questions +
                 '}';
     }
-
 }
